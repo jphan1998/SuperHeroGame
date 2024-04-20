36f37f25262d1b257ef8e80120f1d2a8cafcb76f
jphanGGC <jphan3@ggc.edu>
2024-04-20 13:57:42 -0400

    Added Commands

--- src/Controller/GameController.java
+++ src/Controller/GameController.java
@@ -3,11 +3,15 @@ package Controller;
 import Model.GameModel;
 import View.GameView;
 
+import java.util.Scanner;
+
 public class GameController {
     GameView gameView;
     GameModel gameModel;
+    Scanner in;
 
-    public GameController(GameModel aGameModel, GameView aGameView){
+    public GameController(GameModel aGameModel, GameView aGameView) {
+        in = new Scanner(System.in);
         this.gameModel = aGameModel;
         this.gameView = aGameView;
         gameStart();
@@ -29,8 +33,56 @@ public class GameController {
         this.gameModel = gameModel;
     }
 
-    public void gameStart(){
+    public void gameStart() {
         gameView.welcome();
         gameView.updateView("You are now in " + gameModel.getPlayer().getCurRoom().getName());
+    }
+
+    public void gamePlay(){
+        String verb;
+        String object;
+        String input = in.nextLine();
+        String[] command = input.split(" ", 2);
+        verb = command[0];
+
+        if (command.length == 1) {
+            if (verb.equalsIgnoreCase("n")) {
+
+            } else if (verb.equalsIgnoreCase("e")) {
+
+            } else if (verb.equalsIgnoreCase("S")) {
+
+            } else if (verb.equalsIgnoreCase("W")) {
+
+            } else if (verb.equalsIgnoreCase("Explore")) {
+
+            }
+            else if(verb.equalsIgnoreCase("Inventory")){
+
+            }
+            else if(verb.equalsIgnoreCase("Stats")){
+
+            }
+            else if(verb.equalsIgnoreCase("Un-equip")){
+
+            }
+            else if(verb.equalsIgnoreCase("commands")){
+
+            }
+            else if(verb.equalsIgnoreCase("Exit")){
+                System.exit(0);
+            }
+            else{
+                gameView.wrongCommand();
+            }
+        }
+        if (command.length == 2){
+            object = command[1];
+            if(verb.equalsIgnoreCase("Pickup")){
+            }
+            else {
+
+            }
+        }
     }
 }
--- src/View/GameView.java
+++ src/View/GameView.java
@@ -1,6 +1,14 @@
 package View;
 
 public class GameView {
+    public static final String ANSI_RED = "\u001B[31m";
+
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+
+    public static final String ANSI_GREEN = "\u001B[32m";
+
+    public static final String ANSI_RESET = "\u001B[0m";
 
     public void welcome(){
         System.out.println("Welcome to Dungeon Adventure!");
@@ -8,5 +16,9 @@ public class GameView {
 
     public void updateView(String view){
         System.out.println(view.replaceAll("[.?!]\\s?","$0\\n"));
+    }
+
+    public void wrongCommand(){
+        System.out.println(ANSI_RED + "Please only enter the commands given." + ANSI_RESET);
     }
 }
