a30ddbbaee2fb7c2da03fd0a3af7bc00809ae868
jphanGGC <jphan3@ggc.edu>
2024-04-19 22:58:32 -0400

    Update Player.java

--- src/Model/Player.java
+++ src/Model/Player.java
@@ -1,4 +1,65 @@
 package Model;
 
+import java.util.TreeMap;
+
 public class Player {
+    private Room curRoom;
+    private Room prevRoom;
+    private int HP;
+    private int CR;
+    private Map<String, Item> inventory;
+    private Map<String, Item> gear;
+
+    public Player(){
+        inventory = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+        gear = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+    }
+
+    public Room getCurRoom() {
+        return curRoom;
+    }
+
+    public void setCurRoom(Room curRoom) {
+        this.curRoom = curRoom;
+    }
+
+    public Room getPrevRoom() {
+        return prevRoom;
+    }
+
+    public void setPrevRoom(Room prevRoom) {
+        this.prevRoom = prevRoom;
+    }
+
+    public int getHP() {
+        return HP;
+    }
+
+    public void setHP(int HP) {
+        this.HP = HP;
+    }
+
+    public int getCR() {
+        return CR;
+    }
+
+    public void setCR(int CR) {
+        this.CR = CR;
+    }
+
+    public Map<String, Item> getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(Map<String, Item> inventory) {
+        this.inventory = inventory;
+    }
+
+    public Map<String, Item> getGear() {
+        return gear;
+    }
+
+    public void setGear(Map<String, Item> gear) {
+        this.gear = gear;
+    }
 }
