5856f35b5d7d71c1324cf2b75428e80ba8867c5e
jphanGGC <jphan3@ggc.edu>
2024-04-20 20:42:11 -0400

    Update Consumables.java

--- src/Model/Consumables.java
+++ src/Model/Consumables.java
@@ -6,7 +6,7 @@ public class Consumables extends Item {
 
     int cAmount;
     int cCount;
-    
+
     public Consumables(String itemType, String itemName, String itemDescription, int cAmount, int cCount) {
         super(itemType,itemName, itemDescription);
         this.cAmount = cAmount;
